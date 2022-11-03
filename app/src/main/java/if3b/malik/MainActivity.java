acabb5dcf347295942e9b774643bf85304f3c68e
Maulana Malik <maulanamalik_03@mhs.mdp.ac.id>
2022-11-03 10:40:40 +0700

    up

--- app/src/main/java/if3b/malik/MainActivity.java
+++ app/src/main/java/if3b/malik/MainActivity.java
@@ -1,31 +1,77 @@
   package if3b.malik;
+  import androidx.appcompat.app.AppCompatActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+  import android.content.Intent;
+  import android.os.Bundle;
+  import android.view.View;
+  import android.widget.ArrayAdapter;
+  import android.widget.AutoCompleteTextView;
+  import android.widget.Button;
+  import android.widget.CheckBox;
+  import android.widget.EditText;
+  import android.widget.Spinner;
+  import android.widget.TextView;
+  import android.widget.Toast;
 
   public class MainActivity extends AppCompatActivity {
+      Button btnDaftar;
+      EditText etNama;
+      EditText etNoPendaftaran;
+      CheckBox cbKonfirmasi;
+      AutoCompleteTextView actvJalurPendaftaran;
 
-    Button btnPindah;
+      @Override
+      protected void onCreate(Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
+          setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("LAYOUT A");
+          getSupportActionBar().setTitle("Formulir Pendaftaran");
 
-        btnPindah = findViewById(R.id.btn_daftar);
+          String[] type = new String[] {"Tes Tertulis", "Jalur Tanpa Tes", "Ujian Saingan Masuk"};
+          ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                  this,
+                  R.layout.drop_down_item,
+                  type
+          );
+
+          etNama = findViewById(R.id.et_namalengkap);
+          etNoPendaftaran = findViewById(R.id.et_nomorpendaftaran);
+          actvJalurPendaftaran = findViewById(R.id.atv_jalur_pendaftaran);
+          actvJalurPendaftaran.setAdapter(adapter);
+          cbKonfirmasi = findViewById(R.id.cb_konfirmasi);
+          btnDaftar = findViewById(R.id.btn_daftar);
 
 
+          btnDaftar.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  String Nama = etNama.getText().toString();
+                  String NoPendaftaran = etNoPendaftaran.getText().toString();
+                  String JalurPendaftaran = actvJalurPendaftaran.getText().toString();
 
+                  if (Nama.trim().equals("")) {
+                      etNama.setError("Nama Tidak Boleh Kosong");
+                  }
+                  else if (NoPendaftaran.trim().equals("")) {
+                      etNoPendaftaran.setError("No.Pendaftaran Tidak Boleh Kosong");
+                  }
+                  else if (JalurPendaftaran.trim().isEmpty()) {
+                      Toast.makeText(MainActivity.this, "Pilih Jalur Pendaftaran", Toast.LENGTH_SHORT).show();
+                  }
+                  else {
+                      if (cbKonfirmasi.isChecked()) {
+                          Intent daftar = new Intent(MainActivity.this, SecondActivity.class);
+                          daftar.putExtra("xNama", Nama);
+                          daftar.putExtra("xNoPendaftaran", NoPendaftaran);
+                          daftar.putExtra("xJalurPendaftaran", JalurPendaftaran);
+                          startActivity(daftar);
+                      }
+                      else {
+                          Toast.makeText(MainActivity.this, "Konfirmasi Terlebih Dahulu", Toast.LENGTH_SHORT).show();
+                      }
+                  }
+              }
+          });
 
-    }
-
-      public void Daftar(View view) {
-          Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-          startActivity(intent);
       }
   }
--- app/src/main/java/if3b/malik/SecondActivity.java
+++ app/src/main/java/if3b/malik/SecondActivity.java
@@ -2,19 +2,33 @@ package if3b.malik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
-    Button btnPindah;
+    TextView tvNama;
+    TextView tvNomorPendaftaran;
+    TextView tvJalurPendaftaran;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second2);
+        setContentView(R.layout.activity_second);
         getSupportActionBar().setTitle("LAYOUT B");
 
+        tvNama = findViewById(R.id.tv_nama);
+        tvNomorPendaftaran = findViewById(R.id.tv_no_pendaftaran);
+        tvJalurPendaftaran = findViewById(R.id.tv_jalur_pendaftaran);
+
+        Intent terima = getIntent();
+        String yNama = terima.getStringExtra("xNama");
+        String yNoPendaftaran = terima.getStringExtra("xNoPendaftaran");
+        String yJalurPendaftaran = terima.getStringExtra("xJalurPendaftaran");
+
+        tvNama.setText(yNama);
+        tvNomorPendaftaran.setText(yNoPendaftaran);
+        tvJalurPendaftaran.setText(yJalurPendaftaran);
 
     }
 }
